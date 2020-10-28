89ba609fdfba3226c828cf2dbc655e9376c13b31
Brandon <Brandon@Brandon-GREEN>
2020-10-28 17:04:49 -0400

    just added directory gen (pls test everyone )

---
+++ src/application/CreateNewSaveFileController.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateNewSaveFileController {
+
+	@FXML private Button NextButton, CancelButton;
+	@FXML private TextField ProjectNameTextField;
+	
+	@FXML
+	private void next() {
+		if(isValidProjectName(ProjectNameTextField.getText())) {
+			createProjectDirectory(ProjectNameTextField.getText());
+			try{
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/InitialSetup.fxml"));
+				Parent root = fxmlLoader.load();
+
+				Main.stage.setScene(new Scene(root));
+				Main.stage.show();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}else {
+			this.next();
+		}
+		
+	}
+	
+	private void createProjectDirectory(String projectName) {
+		
+		String path = "C:\\Memoir";
+		File file = new File(path);
+		if(file.mkdir()) {
+			System.out.println("Directory created at " + path);
+		}
+		
+		path = "C:\\Memoir\\" + projectName;
+		file = new File(path);
+		if(file.mkdir()) {
+			System.out.println("Directory created at " + path);
+		}else {
+			System.out.println("mkdir failed --- duplicate project?");
+		}
+	}
+	
+	// THIS IS NOT SUFFICIENT STRING VERIFICATION (Add more l8r)
+	private boolean isValidProjectName(String projectName) {
+		if(projectName.isEmpty()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	@FXML 
+	private void cancel() {
+		System.exit(0);
+	}
+		
+}
